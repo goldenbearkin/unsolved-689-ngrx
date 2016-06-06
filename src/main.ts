f2ecc798f3edc07d63115901185d7f57c80ee16d
jasonhui <huihangkin@gmail.com>
2016-06-06 23:50:32 +0800

    testing

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { provideStore } from '@ngrx/store';
 import { enableProdMode } from '@angular/core';
-import { todosReducer } from './app/shared/ngrx/reducers/todos'
+// import { todosReducer } from './app/shared/ngrx/reducers/todos'
 import { Unsolved689NgrxAppComponent, environment } from './app/';
 
 if (environment.production) {
@@ -10,6 +10,6 @@ if (environment.production) {
 
 bootstrap(Unsolved689NgrxAppComponent, [
   provideStore({ 
-    todos: todosReducer
+    // todos: todosReducer
   })
 ]);
